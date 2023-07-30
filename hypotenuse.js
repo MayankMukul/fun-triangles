dcac66dae98d0f4f26f026248267a887669d0306
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-31 00:37:08 +0530

    initial commit

---
+++ hypotenuse.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector("#btn");
+var base = document.querySelector("#base");
+var height = documert.querySelector("#height");
+
+function calculate(){
+    console.log(base+","+height);
+
+}
+
+btn.addEventListener("click",calculate);
