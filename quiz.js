9bb7faecc906392d4ef3e25da39222b6727b4b54
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-31 00:00:07 +0530

    quiz page done

--- quiz.js
+++ quiz.js
@@ -2,20 +2,20 @@
 var answer = ['90','right'];
 var btn = document.querySelector("#btn");
 var qform = document.querySelector(".quizform");
-
+var message = document.querySelector(".message");
 function display(){
    
    var score = 0;
    var index =0;
     const qdata = new FormData(qform);
-    console.log(qdata);
     for (let value of qdata.values() ){
         if (value ===answer[index]){
             score++;
         }
         index++;
     }
-    console.log("your score "+ score);
+
+    message.innerText="your score is : "+score;
 }
 
 btn.addEventListener("click",display);
