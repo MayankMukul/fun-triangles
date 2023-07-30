3fb2b0adcc49519d8fdc078ccdb6b19a8a1ce279
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-30 18:28:07 +0530

    quiz page

--- quiz.js
+++ quiz.js
@@ -1,4 +1,18 @@
-var input1 = document.querySelector(".option1");
-var input2 = document.querySelector("option2");
+
 var answer = ['90','right'];
-var btn = document.querySelector(".btn")
+var btn = document.querySelector("#btn");
+var form = document.querySelector(".quizform");
+
+function display(){
+   
+   var score = 0;
+   var index =0;
+    const data = new FormData(form);
+    console.log(data);
+    for (let item in data.entries ){
+        console.log(item);
+    }
+
+}
+
+btn.addEventListener("click",display);
