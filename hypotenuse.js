cfd9eaf6ff67929e7c400ed33c4c2dbf6e60def1
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-31 11:08:26 +0530

    hypotenuse js done

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,10 +1,11 @@
 var btn = document.querySelector("#btn");
 var base = document.querySelector("#base");
 var height = document.querySelector("#height");
+var message = document.querySelector(".output");
 
 function calculate(){
-    console.log(base.value+","+height.value);
-
+    var h;
+    h = Math.sqrt((base.value * base.value) + (height.value * height.value));
+    message.innerText = "The length of the hypotenuse is " + h ;
 }
-
 btn.addEventListener("click",calculate);
