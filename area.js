ac18f0b83a9c1e65458cd15c000e7300c780b83b
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-31 12:09:49 +0530

    basic area page

---
+++ area.js
@@ -0,0 +1,21 @@
+var base1 = document.querySelector("#base1");
+var height1 = document.querySelector("#height1");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var output = document.querySelector(".output");
+var lengths = document.querySelectorAll(".length");
+
+function area1() {
+    var area = 0.5 * (base1.value *height1.value) ;
+    output.innerText = "the area of the triangle whose base "+base1.value+" and height "+height1.value+" is "+area;
+}
+
+function area2 ( ){
+    var s = (lengths[0].value + lengths[1].value + lengths[2].value)/2;
+    var area = Math.sqrt(s*(s-lengths[0].value)*(s-lengths[1].value)*(s-lengths[2].value));
+    output.innerText = `the area of the triangle with sides ${lengths[0].value}, ${lengths[1].value}, ${lengths[2].value} is ${area}`;
+
+}
+
+btn1.addEventListener("click",area1);
+btn2.addEventListener("click",area2);
