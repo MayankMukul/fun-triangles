b2abaac80e512d111009ecaffd64156c432bda3d
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-07 23:57:04 +0530

    update

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 
-var answer = ['90','right'];
+var answer = ['90','right','one'];
 var btn = document.querySelector("#btn");
 var qform = document.querySelector(".quizform");
 var message = document.querySelector("#message");
@@ -7,15 +7,20 @@ function display(){
    
    var score = 0;
    var index =0;
+
+   if (qform !== ''){
     const qdata = new FormData(qform);
     for (let value of qdata.values() ){
         if (value ===answer[index]){
             score++;
         }
         index++;
-    }
-
+    } 
     message.innerText="You scored "+score + " points.";
+   }else {
+    message.innerText = ('Please fill the form');
+}
+   
 }
 
 btn.addEventListener("click",display);
