ba701cfe374130a65304e70bc837a490734dc65e
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-08 11:46:38 +0530

    fixed to two decinmal

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,7 +7,7 @@ function calculate(){
     if (base.value !=='' && height.value !==''){
         var h;
         h = Math.sqrt((base.value * base.value) + (height.value * height.value));
-        message.innerHTML = `<span style="color : green">The length of Hypotenuse is ${h} </span>`;
+        message.innerHTML = `<span style="color : green">The length of Hypotenuse is ${h.toFixed(2)} </span>`;
     } else if (base.value == '' && height.value == ''){
         message.innerText = `*Enter lenght of base and height first`;
     }
